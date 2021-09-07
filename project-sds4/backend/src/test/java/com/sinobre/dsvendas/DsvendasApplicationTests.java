cbe4e7a3a9edc9f24a73ff8b8a7585bfc29a1031
FabioSinobre <engenheiro.computacao4.0@gmail.com>
2021-09-06 22:52:12 -0300

    Base editada

---
+++ project-sds4/backend/src/test/java/com/sinobre/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinobre.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
