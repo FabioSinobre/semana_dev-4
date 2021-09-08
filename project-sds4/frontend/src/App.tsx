c2c2659b7f55970709aba8bc031e0c6eb1bcb42d
FabioSinobre <engenheiro.computacao4.0@gmail.com>
2021-09-07 22:30:16 -0300

    up

--- project-sds4/frontend/src/App.tsx
+++ project-sds4/frontend/src/App.tsx
@@ -1,8 +1,9 @@
-
+import NavBar from"components/navBar";
 function App() {
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <h1 className="text-primary">Hola Mundo</h1>
+	  <NavBar />
     </div>
   );
 }
---
+++ project-sds4/frontend/src/components/navBar/index.tsx
@@ -0,0 +1,9 @@
+function NavBar() {
+	return (
+		<div>
+			<h2 className="text-primary">DevSuperior</h2>
+		</div>
+	);
+}
+
+export default NavBar;
