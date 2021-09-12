a48e81722959464783deb9c105077f16ac8b0917
FabioSinobre <engenheiro.computacao4.0@gmail.com>
2021-09-12 17:53:52 -0300

    bootstrap

--- project-sds4/frontend/src/App.tsx
+++ project-sds4/frontend/src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Nova Versão limpa e atualizada</h1>
+      <h1 className="text-primary">Nova Versão limpa e atualizada</h1>
     </div>
   );
 }
