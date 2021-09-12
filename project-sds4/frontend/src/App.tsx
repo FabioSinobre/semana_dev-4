bdba537f323e57fd9466e1117f49bfc4a85aa6c4
FabioSinobre <engenheiro.computacao4.0@gmail.com>
2021-09-12 13:59:42 -0300

    created
    Projeto corrigido

--- project-sds4/frontend/src/App.tsx
+++ project-sds4/frontend/src/App.tsx
@@ -1,9 +1,24 @@
-import NavBar from"components/navBar";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Hola Mundo</h1>
-	  <NavBar />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- project-sds4/frontend/src/components/navBar/index.tsx
+++
@@ -1,9 +0,0 @@
-function NavBar() {
-	return (
-		<div>
-			<h2 className="text-primary">DevSuperior</h2>
-		</div>
-	);
-}
-
-export default NavBar;
