5e946d33f73c21919fd096b76d99ac481d8ff903
FabioSinobre <engenheiro.computacao4.0@gmail.com>
2021-09-12 20:28:05 -0300

    Static charts
    pagina estatica concluida

--- project-sds4/frontend/src/components/Footer/index.tsx
+++ project-sds4/frontend/src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Fabio Sinobre</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                    Evento promovido pela escola DevSuperior: codigo disponivel no github <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">FabioSinobre</a></small></p>
             </div>
         </footer>
 
