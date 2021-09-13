5f139b75da02c1820095562055c061e84151a424
FabioSinobre <engenheiro.computacao4.0@gmail.com>
2021-09-13 20:12:44 -0300

    Update index.tsx

--- project-sds4/frontend/src/components/Footer/index.tsx
+++ project-sds4/frontend/src/components/Footer/index.tsx
@@ -3,9 +3,9 @@ const Footer = () => {
 
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Fabio Sinobre</a></p>
+                <p className="text-light">App desenvolvido por <a href="http://www.linkedin.com/in/fábio-sinobre" target="_blank" rel="noreferrer">Fabio Sinobre</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Evento promovido pela escola DevSuperior: codigo disponivel no github <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">FabioSinobre</a></small></p>
+                    Evento promovido pela escola DevSuperior: codigo disponivel no github <a href="https://github.com/FabioSinobre" target="_blank" rel="noreferrer">FabioSinobre</a></small></p>
             </div>
         </footer>
 
