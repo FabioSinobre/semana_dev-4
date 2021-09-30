82172437818b1654a1d64342f90860c6b29140ef
FabioSinobre <engenheiro.computacao4.0@gmail.com>
2021-09-30 20:22:26 -0300

    Layers

---
+++ project-sds4/backend/src/main/java/com/sinobre/dsvendas/controllers/SellerController.java
@@ -0,0 +1,27 @@
+package com.sinobre.dsvendas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sinobre.dsvendas.dto.SellerDTO;
+import com.sinobre.dsvendas.service.SellerService;
+
+@RestController
+@RequestMapping(value="/sellers")
+public class SellerController {
+	
+	@Autowired
+	private SellerService service;
+	
+	@GetMapping
+	public ResponseEntity<List<SellerDTO>> findAll(){
+		List<SellerDTO> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+	
+}
